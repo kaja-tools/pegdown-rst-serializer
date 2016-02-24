5cdf5f991c1dfaeccaf59ab8df0cc2bf196aaeef
Tomas Vesely <tomas.vesely@skype.net>
2016-02-25 00:05:20 +0100

    NFR

--- src/main/java/tools/kaja/pegdown/table/Cell.java
+++ src/main/java/tools/kaja/pegdown/table/Cell.java
@@ -1,20 +1,41 @@
 package tools.kaja.pegdown.table;
 
+/**
+ * Table cell
+ */
 public class Cell
 {
 
+    /**
+     * Inner reStructuredText markup
+     */
     protected String rst = "";
 
+    /**
+     * Get inner reStructuredText markup
+     *
+     * @return reStructuredText markup
+     */
     public String getRst()
     {
         return rst;
     }
 
+    /**
+     * Set inner reStructuredText markup
+     *
+     * @param rst reStructuredText markup
+     */
     public void setRst( String rst )
     {
         this.rst = rst;
     }
 
+    /**
+     * Get width (length)
+     *
+     * @return String length of the inner reStructuredText markup
+     */
     public int getWidth()
     {
         return rst.length();
--- src/main/java/tools/kaja/pegdown/table/Column.java
+++ src/main/java/tools/kaja/pegdown/table/Column.java
@@ -3,23 +3,52 @@ package tools.kaja.pegdown.table;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Table column
+ *
+ * This class represents a vertical group of cells. The primary goal
+ * is to calculate it's width. Column width is calculated as a max width among all the cells.
+ */
 public class Column
 {
 
+    /**
+     * List of cells
+     */
     protected List<Cell> cells = new ArrayList<Cell>();
+
+    /**
+     * Column width
+     */
     protected Integer width;
 
+    /**
+     * Add a cell
+     *
+     * @param cell Cell
+     */
     public void addCell( Cell cell )
     {
         cells.add( cell );
         width = null;
     }
 
+    /**
+     * Number of cells in this column
+     *
+     * @return Number of cells in this column
+     */
     public int size()
     {
         return cells.size();
     }
 
+    /**
+     * Get width
+     *
+     * Width is calculated as a max width among all the cells.
+     * @return Width of this column
+     */
     public int getWidth()
     {
         if ( width == null )
@@ -30,6 +59,9 @@ public class Column
         return width;
     }
 
+    /**
+     * Load width
+     */
     protected void loadWidth()
     {
         width = 0;
