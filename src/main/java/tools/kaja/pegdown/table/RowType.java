fbdc5a38abc266407b23e514e473c7e0502dd48a
Tomas Vesely <tomas.vesely@skype.net>
2016-02-25 00:48:25 +0100

    NFR

--- src/main/java/tools/kaja/pegdown/table/RowType.java
+++ src/main/java/tools/kaja/pegdown/table/RowType.java
@@ -1,6 +1,20 @@
 package tools.kaja.pegdown.table;
 
+/**
+ * Table row type
+ *
+ * Row type affects the used reStructuredText underline character. Character "=" is used for HEADER
+ * and "-" for BODY.
+ */
 public enum RowType
 {
-    HEADER, BODY
+    /**
+     * Similar to HTML <th>. Underlined with "=".
+     */
+    HEADER,
+
+    /**
+     * Similar to HTML <tr>. Underlined with "-".
+     */
+    BODY
 }
--- src/main/java/tools/kaja/pegdown/table/Table.java
+++ src/main/java/tools/kaja/pegdown/table/Table.java
@@ -3,30 +3,70 @@ package tools.kaja.pegdown.table;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Table
+ *
+ * reStructuredText is using space-sensitive table painting syntax. It cannot be written at the same time
+ * as the Markdown parser is reading the source. This class represents an intermediate structure that is
+ * built by the parser and then written by TableRstWritter.
+ */
 public class Table
 {
 
+    /**
+     * List of rows
+     */
     protected List<Row> rows = new ArrayList<Row>();
+
+    /**
+     * List of columns
+     */
     protected List<Column> columns;
+
+    /**
+     * Active (last created) row
+     */
     protected Row activeRow;
+
+    /**
+     * Type of the next created row
+     */
     protected RowType nextRowType = RowType.BODY;
 
+    /**
+     * Set type of the next created row(s)
+     *
+     * @param type Type of the row
+     */
     public void setNextRowType( RowType type )
     {
         nextRowType = type;
     }
 
+    /**
+     * Add a new row
+     */
     public void addRow()
     {
         activeRow = new Row( nextRowType );
         rows.add( activeRow );
     }
 
+    /**
+     * Get list of rows
+     *
+     * @return List of rows
+     */
     public List<Row> getRows()
     {
         return rows;
     }
 
+    /**
+     * Add a new cell
+     *
+     * @return Newly created cell
+     */
     public Cell addCell()
     {
         columns = null;
@@ -39,6 +79,11 @@ public class Table
         return activeRow.addCell();
     }
 
+    /**
+     * Get list of columns
+     *
+     * @return List of columns
+     */
     public List<Column> getColumns()
     {
         if ( columns == null )
@@ -49,6 +94,11 @@ public class Table
         return columns;
     }
 
+    /**
+     * Load columns
+     *
+     * Columns are lazy loaded. Every call to addCell() cleans the cache.
+     */
     protected void loadColumns()
     {
         columns = new ArrayList<Column>();
@@ -68,6 +118,13 @@ public class Table
         }
     }
 
+    /**
+     * Get column at specified index
+     *
+     * Columns are indexed from 0.
+     * @param index Column index
+     * @return Column or null if it does not exist
+     */
     protected Column getColumn( int index )
     {
         Column column = new Column();
