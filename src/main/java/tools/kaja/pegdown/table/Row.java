2eeb797bd425777ac7549d78e54b87016ab24a3a
Tomas Vesely <tomas.vesely@skype.net>
2016-02-25 00:26:00 +0100

    NFR

--- src/main/java/tools/kaja/pegdown/table/Row.java
+++ src/main/java/tools/kaja/pegdown/table/Row.java
@@ -3,23 +3,53 @@ package tools.kaja.pegdown.table;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Table row
+ */
 public class Row
 {
 
+    /**
+     * Type of this row (header or body)
+     */
     protected RowType type;
+
+    /**
+     * List of cells
+     */
     protected List<Cell> cells = new ArrayList<Cell>();
+
+    /**
+     * Active cell
+     */
     protected Cell activeCell;
 
+    /**
+     * Constructor
+     *
+     * @param type Type of this row
+     */
     public Row( RowType type )
     {
         this.type = type;
     }
 
+    /**
+     * Get type
+     *
+     * Header or body. Similar to HTML <th> and <tr>.
+     * @return Type of this row
+     */
     public RowType getType()
     {
         return type;
     }
 
+    /**
+     * Add a new cell
+     *
+     * @return Newly created cell
+     */
     public Cell addCell()
     {
         activeCell = new Cell();
@@ -28,17 +58,20 @@ public class Row
         return activeCell;
     }
 
+    /**
+     * Get cell at specified index (position)
+     *
+     * Cells are indexed from 0.
+     * @param index Index (position)
+     * @return Cell at given index or null if it does not exist
+     */
     public Cell getCell( int index )
     {
-        try
+        if ( index >= cells.size() )
         {
-            return cells.get( index );
-        }
-        catch ( IndexOutOfBoundsException e )
-        {
-
+            return null;
         }
 
-        return null;
+        return cells.get( index );
     }
 }
